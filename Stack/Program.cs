de335a34e1d5fc30e26411d345e79577ee396c4b
hpharis258 <60554302+hpharis258@users.noreply.github.com>
2022-10-05 22:57:01 +0100

    Implemented a Stack

---
+++ Stack/Program.cs
@@ -0,0 +1,27 @@
+﻿// Stack Implementation in C#
+// Using Generics 
+// Underlying Implementation is an Array
+// By Haroldas Varanauskas
+namespace Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 5;
+            Stack<string> testStack = new Stack<string>(size);
+            testStack.push("Test String ");
+            testStack.push("Second Item");
+            testStack.push("Third Item");
+            Console.WriteLine("Is this stack Full?");
+            Console.WriteLine(testStack.isFull().ToString());
+            Console.WriteLine("Peek On the Stack!");
+            Console.WriteLine(testStack.peek().ToString());
+            Console.WriteLine("Pop the Stack!!!");
+            testStack.pop();
+            Console.WriteLine("Peek On the Stack!");
+            Console.WriteLine(testStack.peek().ToString());
+            Console.WriteLine(testStack.isEmpty().ToString());
+        }
+    }
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class Stack<T>
+    {
+
+        public int size;
+        public int top = -1;
+        public T[] stack;
+
+        // Constructor
+        public Stack(int size)
+        {
+            this.size = size;
+            this.stack = new T[size];
+        }
+
+        // Is full method
+        public bool isFull()
+        {
+            if(this.top == this.size - 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        // If Stack is Empty Return True else False
+        public bool isEmpty()
+        {
+            if(this.top == -1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void push(T data)
+        {
+            // Method To Push the DATA:
+            if(isFull() == true)
+            {
+                Console.WriteLine("The stack is full ");
+            }
+            else
+            {
+                this.top++;
+                this.stack[this.top] = data;
+            }
+
+        }
+        // Pop Method 
+        public T pop()
+        {
+            //
+            if (isEmpty() == true)
+            {
+                Console.WriteLine("The Stack is Empty");
+                return default;
+            }
+            else
+            {
+                T poppedItem = this.stack[this.top];
+                this.top--;
+                return poppedItem;
+            }
+        }
+        // Peek Method
+        public T peek()
+        {
+            if (isEmpty() == true)
+            {
+                Console.WriteLine("The Stack is Empty");
+                return default;
+            }
+            else
+            {
+                T itemCopy = this.stack[this.top];
+                return itemCopy;
+            }
+        }
+    }
+}
