e96939c71b029026a80204c2b8fe0de3dbd7ca51
hpharis258 <60554302+hpharis258@users.noreply.github.com>
2022-10-08 21:51:09 +0100

    Added Dynamic Array

---
+++ Dynamic Array/DynamicArray/DynamicArray.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicArray
+{
+    public class DynamicArray<T>
+    {
+        // Initialise Array of the type that was given
+        public T[] array;
+        //
+        int capacity = 0;
+        //constructor
+        public DynamicArray(int capacity)
+        {
+            // Check capacity
+            if(capacity < 0)
+            {
+                throw new Exception("Capacity Must be more than 0");
+            }
+            this.capacity = capacity;
+            this.array = new T[capacity];
+        }
+        // Set Data at a given Index
+        public void set(int index, T data)
+        {
+            if(index == this.capacity - 1)
+            {
+                this.capacity = this.capacity * 2;
+                T [] newArray = new T[this.capacity];
+                this.array.CopyTo(newArray, 1);
+                this.array = newArray;
+            }
+            this.array[index] = data;
+        }
+        // Get Data
+        public T? get(int index)
+        {
+            return array[index];
+        }
+    }
+}
---
+++ Dynamic Array/DynamicArray/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace DynamicArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DynamicArray<String> Test = new DynamicArray<string>(5);
+            Test.set(0, "ITEM 1");
+            Test.set(1, "ITEM 2");
+            Test.set(2, "ITEM 3");
+            Test.set(3, "ITEM 4");
+            Test.set(4, "ITEM 5");
+            Test.set(5, "ITEM 6");
+            Test.set(6, "ITEM 7");
+            Console.WriteLine(Test.get(0));
+            Console.WriteLine(Test.get(1));
+            Console.WriteLine(Test.get(2));
+            Console.WriteLine(Test.get(3));
+            Console.WriteLine(Test.get(4));
+            Console.WriteLine(Test.get(5));
+            Console.WriteLine(Test.get(6));
+        }
+    }
+}
